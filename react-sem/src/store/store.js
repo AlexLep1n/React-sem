c8fa1091a552ec4a0f41a20cba405f589f17b3d2
Alex Lepin <alexlepin@mail.ru>
2024-03-09 22:13:40 +0300

    init: initial commit

--- react-sem/src/store/store.js
+++ react-sem/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../reducers/CounterSlice.js";
 import todoListReducer from "../reducers/TodoListSlice.js";
+import favoritesSlice from "../reducers/favoritesSlice .js";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     todoList: todoListReducer,
+    favList: favoritesSlice,
   },
 });
